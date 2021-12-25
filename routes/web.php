000c56b94b64bdc923255f0767c1b1f516a751d3
rez120 <r.pishkar121@gmail.com>
2021-12-25 15:42:17 +0330

    added articles table

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('/', function(){
     return view('welcome');
+});
+
+Route::prefix('/prefix')->group(function(){
+    Route::get('/', function (){
+        return 'hellow';
+    });
+
+    Route::get('/salve', function (){
+        return 'salutor';
+    });
 });
